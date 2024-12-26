bb585e7802ad03c5c343d5ecc707611fe03f36e0
Eugene <eizeed321@mgail.com>
2024-12-26 10:20:20 +0500

    added better gitignore parse

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::{Path, PathBuf}};
+use std::{io::Write, collections::HashMap, fs, path::{Path, PathBuf}};
 
 use clap::Parser;
 use env_logger::Builder;
@@ -23,9 +23,16 @@ fn get_gitignore(dir: &Path) -> Vec<String> {
             let path = entry.path();
 
             if path.file_name().unwrap_or_default().to_str().unwrap_or("") == ".gitignore" {
+                let start_regex = Regex::new(r#"^[/\- \t]"#).unwrap();
+                let end_regex = Regex::new(r#"[/ \t]+$"#).unwrap();
                 let lines = fs::read_to_string(&path).unwrap();
                 let lines = lines.lines();
                 for line in lines {
+                    if line.starts_with("#") {
+                        continue;
+                    }
+                    let line = start_regex.replace_all(&line, "");
+                    let line = end_regex.replace_all(&line, "");
                     gitignore.push(line.to_string())
                 }
                 break 'outer;
@@ -36,12 +43,17 @@ fn get_gitignore(dir: &Path) -> Vec<String> {
     return gitignore;
 }
 
-fn visit_dir(dir: &Path, ext: &str, gitignore: &mut Vec<String>) -> std::io::Result<usize> {
+fn visit_dir<'a>(dir: &Path, ext: &str, gitignore_map: &mut HashMap<PathBuf, Vec<String>>) -> std::io::Result<usize> {
     let mut lines = 0usize;
     if dir.is_dir() {
-        log::info!("Old gitignore passed into {:?} dir: {:?}", dir.file_name(), gitignore);
-        gitignore.append(&mut get_gitignore(dir));
-        log::info!("Added new Gitignore in new dir: {:?}", gitignore);
+        log::debug!("Old gitignore passed into {:?} dir: {:?}", dir.file_name(), gitignore_map);
+        let ignore_vec = get_gitignore(dir);
+        if ignore_vec.len() > 0 {
+            gitignore_map.insert(dir.to_path_buf().clone(),get_gitignore(dir));
+            log::debug!("Added new Gitignore in new dir: {:?}", gitignore_map);
+        } else {
+            log::debug!("No gitignore in dir: {:?}", dir.file_name());
+        }
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
@@ -51,7 +63,7 @@ fn visit_dir(dir: &Path, ext: &str, gitignore: &mut Vec<String>) -> std::io::Res
             }
 
             
-            let contains = gitignore.iter().any(|s| s == path.file_name().unwrap().to_str().unwrap());
+            let contains = gitignore_map.iter().any(|(_k, v)| v.contains(&path.file_name().unwrap().to_str().unwrap().to_string()));
 
             if contains {
                 log::info!("Ignored file: {:?}", path.file_name().unwrap());
@@ -60,11 +72,11 @@ fn visit_dir(dir: &Path, ext: &str, gitignore: &mut Vec<String>) -> std::io::Res
 
             if path.is_dir() {
                 log::info!("Dir name {:?}", path.file_name().unwrap());
-                lines += visit_dir(&path, ext, gitignore)?;
+                lines += visit_dir(&path, ext, gitignore_map)?;
             } else {
                 if path.file_name().unwrap().to_str().unwrap().ends_with(ext) {
-                    log::info!("Good file with good ext");
-                    log::info!("Filename name {:?}", path.file_name().unwrap());
+                    log::debug!("Good file with good ext");
+                    log::debug!("Filename name {:?}", path.file_name().unwrap());
                     lines += count_lines(&path);
                 } else {
                     continue;
@@ -72,9 +84,10 @@ fn visit_dir(dir: &Path, ext: &str, gitignore: &mut Vec<String>) -> std::io::Res
             }
             log::info!("Total amount of lines: {}\n", &lines);
         }
+        gitignore_map.remove(&dir.to_path_buf());
     }
     log::info!("Getting out of {:?}", dir.file_name());
-    log::info!("Total lines in {:?}: {}",dir.file_name(), lines);
+    log::info!("Total lines in {:?}: {}\n",dir.file_name(), lines);
     Ok(lines)
 }
 
@@ -83,14 +96,21 @@ fn count_lines(file: &Path) -> usize {
 
     let new_lines_re = Regex::new(r#"\n{2,}"#).unwrap();
     let multi_comment = Regex::new(r#"/\*[\s\S]*\*/\s"#).unwrap();
+    let single_comment = Regex::new(r#"\s*//.*"#).unwrap();
 
     let file_str = new_lines_re.replace_all(&file_str, "\n");
     let file_str = multi_comment.replace_all(&file_str, "");
     let mut lines: Vec<&str> = file_str.lines().collect();
-    for (i, line) in lines.clone().into_iter().enumerate() {
-        if line.starts_with("//") {
+
+    let mut i = 0;
+    let mut len = lines.len();
+    while i < len {
+        if single_comment.is_match(lines[i]) {
             lines.remove(i);
+            len -= 1;
+            continue;
         }
+        i += 1;
     }
     log::info!("Lines in {:?}: {}",file.file_name(), lines.clone().len());
     lines.len()
@@ -99,9 +119,18 @@ fn count_lines(file: &Path) -> usize {
 fn main() {
     let cli = Cli::parse();
 
+
     if cli.verbose {
         Builder::new()
             .filter(None, log::LevelFilter::Info)
+            // .format(|buf, record| {
+            //     let info_style = buf.default_level_style(log::Level::Info);
+            //     writeln!(
+            //         buf,
+            //         "INFO: {info_style:#}{}{info_style}",
+            //         record.args()
+            //     )
+            // })
             .init();
     } else {
         Builder::new()
@@ -111,11 +140,15 @@ fn main() {
 
     match fs::metadata(&cli.path) {
         Ok(metadata) => {
-            if metadata.is_dir() { 
+            if metadata.is_dir() {
                 log::info!("Path: {}", cli.path.to_str().unwrap());
                 log::info!("File extension: {}", cli.extension);
-                let lines = visit_dir(&cli.path, &cli.extension, &mut vec![]);
-                println!("{}", lines.unwrap());
+                let mut gitignore_map: HashMap<PathBuf, Vec<String>>= HashMap::new();
+                let lines = visit_dir(&cli.path, &cli.extension, &mut gitignore_map);
+
+                // Force to use white color in terminal
+                // because logger makes it green
+                println!("\x1b[0m{}\x1b[0m", lines.unwrap());
             }
         },
         Err(_) => ()
